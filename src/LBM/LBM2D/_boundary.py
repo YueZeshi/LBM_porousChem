caa44c18d0417963275b53b2157d009aca865457
Sylvain <609190542@qq.com>
2026-01-21 13:22:51 +0800

    update 2026.02.02

--- src/LBM/LBM2D/_boundary.py
+++ src/LBM/LBM2D/_boundary.py
@@ -53,76 +53,80 @@ class LBM2D_BOUNDARY(LBM2D_BASE):
     # AA pattern 奇数步本格，偶数步邻居格
     @ti.func
     def Boundary_condition_flow_NEE_0(self,x,y,z):
-        if ti.static(self.bc_v[0]==BC.fixedValue):
-            self.v[0,y,z] = self.v_bc_profile[0][0,y,z]
-        elif ti.static(self.bc_v[0]==BC.zeroGradient):
-            self.v[0,y,z] = self.v[1,y,z]
-        if ti.static(self.bc_rho[0]==BC.fixedValue):
-            self.rho[0,y,z] = self.rho_bc_profile[0][0,y,z]
-        elif ti.static(self.bc_rho[0]==BC.zeroGradient):
-            self.rho[0,y,z] = self.rho[1,y,z]#2*self.rho[1,y,z]-self.rho[2,y,z]
-        if self.even_step[None]==0: # 偶数步
-            for s in ti.static([1,5,8]):
-                i_storage = self.periodic_index([0,y,z]+self.e9[s])
-                i_neighbor_storage = self.periodic_index([1,y,z]+self.e9[s])
-                self.f[i_storage][self.LR[s]] = self.feq9(s,0,y,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,1,y,z))
-        else:  # 奇数步
-            for s in ti.static([1,5,8]):
-                self.f[0,y,z][s] = self.feq9(s,0,y,z)+(self.f[1,y,z][s]-self.feq9(s,1,y,z))
+        if self.solid[0,y,z]<1:
+            if ti.static(self.bc_v[0]==BC.fixedValue):
+                self.v[0,y,z] = self.v_bc_profile[0][0,y,z]
+            elif ti.static(self.bc_v[0]==BC.zeroGradient):
+                self.v[0,y,z] = self.v[1,y,z]
+            if ti.static(self.bc_rho[0]==BC.fixedValue):
+                self.rho[0,y,z] = self.rho_bc_profile[0][0,y,z]
+            elif ti.static(self.bc_rho[0]==BC.zeroGradient):
+                self.rho[0,y,z] = self.rho[1,y,z]#2*self.rho[1,y,z]-self.rho[2,y,z]
+            if self.even_step[None]==0: # 偶数步
+                for s in ti.static([1,5,8]):
+                    i_storage = self.periodic_index([0,y,z]+self.e9[s])
+                    i_neighbor_storage = self.periodic_index([1,y,z]+self.e9[s])
+                    self.f[i_storage][self.LR[s]] = self.feq9(s,0,y,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,1,y,z))
+            else:  # 奇数步
+                for s in ti.static([1,5,8]):
+                    self.f[0,y,z][s] = self.feq9(s,0,y,z)+(self.f[1,y,z][s]-self.feq9(s,1,y,z))
     @ti.func
     def Boundary_condition_flow_NEE_1(self,x,y,z):
-        if ti.static(self.bc_v[1]==BC.fixedValue):
-            self.v[self.nx-1,y,z] = self.v_bc_profile[1][0,y,z]
-        elif ti.static(self.bc_v[1]==BC.zeroGradient):
-            self.v[self.nx-1,y,z] = self.v[self.nx-2,y,z]#2*self.v[self.nx-2,y,z]-self.v[self.nx-3,y,z]
-        if ti.static(self.bc_rho[1]==BC.fixedValue):
-            self.rho[self.nx-1,y,z] = self.rho_BC[1]
-        elif ti.static(self.bc_rho[1]==BC.zeroGradient):
-            self.rho[self.nx-1,y,z] = self.rho[self.nx-2,y,z]#2*self.rho[self.nx-2,y,z]-self.rho[self.nx-3,y,z]#self.rho[self.nx-2,y,z]#
-        if self.even_step[None]==0: # 偶数步
-            for s in ti.static([3,6,7]):
-                i_storage = self.periodic_index([self.nx-1,y,z]+self.e9[s])
-                i_neighbor_storage = self.periodic_index([self.nx-2,y,z]+self.e9[s])
-                self.f[i_storage][self.LR[s]] = self.feq9(s,self.nx-1,y,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,self.nx-2,y,z))
-        else:  # 奇数步
-            for s in ti.static([3,6,7]):
-                self.f[self.nx-1,y,z][s] = self.feq9(s,self.nx-1,y,z)+(self.f[self.nx-2,y,z][s]-self.feq9(s,self.nx-2,y,z))
+        if self.solid[self.nx-1,y,z]<1:
+            if ti.static(self.bc_v[1]==BC.fixedValue):
+                self.v[self.nx-1,y,z] = self.v_bc_profile[1][0,y,z]
+            elif ti.static(self.bc_v[1]==BC.zeroGradient):
+                self.v[self.nx-1,y,z] = self.v[self.nx-2,y,z]#2*self.v[self.nx-2,y,z]-self.v[self.nx-3,y,z]
+            if ti.static(self.bc_rho[1]==BC.fixedValue):
+                self.rho[self.nx-1,y,z] = self.rho_BC[1]
+            elif ti.static(self.bc_rho[1]==BC.zeroGradient):
+                self.rho[self.nx-1,y,z] = self.rho[self.nx-2,y,z]#2*self.rho[self.nx-2,y,z]-self.rho[self.nx-3,y,z]#self.rho[self.nx-2,y,z]#
+            if self.even_step[None]==0: # 偶数步
+                for s in ti.static([3,6,7]):
+                    i_storage = self.periodic_index([self.nx-1,y,z]+self.e9[s])
+                    i_neighbor_storage = self.periodic_index([self.nx-2,y,z]+self.e9[s])
+                    self.f[i_storage][self.LR[s]] = self.feq9(s,self.nx-1,y,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,self.nx-2,y,z))
+            else:  # 奇数步
+                for s in ti.static([3,6,7]):
+                    self.f[self.nx-1,y,z][s] = self.feq9(s,self.nx-1,y,z)+(self.f[self.nx-2,y,z][s]-self.feq9(s,self.nx-2,y,z))
     @ti.func
     def Boundary_condition_flow_NEE_2(self,x,y,z):
-        if ti.static(self.bc_v[2]==BC.fixedValue):
-            self.v[x,0,z] = self.v_bc_profile[2][x,0,z]
-        elif ti.static(self.bc_v[2]==BC.zeroGradient):
-            self.v[x,0,z] = self.v[x,1,z]#2*self.v[x,1,z]-self.v[x,2,z]
-        if ti.static(self.bc_rho[2]==BC.fixedValue):
-            self.rho[x,0,z] = self.rho_BC[2]
-        elif ti.static(self.bc_rho[2]==BC.zeroGradient):
-            self.rho[x,0,z] = self.rho[x,1,z]#2*self.rho[x,1,z]-self.rho[x,2,z]
-        if self.even_step[None]==0: # 偶数步
-            for s in ti.static([2,5,6]):
-                i_storage = self.periodic_index([x,0,z]+self.e9[s])
-                i_neighbor_storage = self.periodic_index([x,1,z]+self.e9[s])
-                self.f[i_storage][self.LR[s]] = self.feq9(s,x,0,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,x,1,z))
-        else:  # 奇数步
-            for s in ti.static([2,5,6]):
-                self.f[x,0,z][s] = self.feq9(s,x,0,z)+(self.f[x,1,z][s]-self.feq9(s,x,1,z))
+        if self.solid[x,0,z]<1:
+            if ti.static(self.bc_v[2]==BC.fixedValue):
+                self.v[x,0,z] = self.v_bc_profile[2][x,0,z]
+            elif ti.static(self.bc_v[2]==BC.zeroGradient):
+                self.v[x,0,z] = self.v[x,1,z]#2*self.v[x,1,z]-self.v[x,2,z]
+            if ti.static(self.bc_rho[2]==BC.fixedValue):
+                self.rho[x,0,z] = self.rho_BC[2]
+            elif ti.static(self.bc_rho[2]==BC.zeroGradient):
+                self.rho[x,0,z] = self.rho[x,1,z]#2*self.rho[x,1,z]-self.rho[x,2,z]
+            if self.even_step[None]==0: # 偶数步
+                for s in ti.static([2,5,6]):
+                    i_storage = self.periodic_index([x,0,z]+self.e9[s])
+                    i_neighbor_storage = self.periodic_index([x,1,z]+self.e9[s])
+                    self.f[i_storage][self.LR[s]] = self.feq9(s,x,0,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,x,1,z))
+            else:  # 奇数步
+                for s in ti.static([2,5,6]):
+                    self.f[x,0,z][s] = self.feq9(s,x,0,z)+(self.f[x,1,z][s]-self.feq9(s,x,1,z))
     @ti.func
     def Boundary_condition_flow_NEE_3(self,x,y,z):
-        if ti.static(self.bc_v[3]==BC.fixedValue):
-            self.v[x,self.ny-1,z] = self.v_bc_profile[3][x,0,z]
-        elif ti.static(self.bc_v[3]==BC.zeroGradient):
-            self.v[x,self.ny-1,z] = self.v[x,self.ny-2,z]#2*self.v[x,self.ny-2,z]-self.v[x,self.ny-3,z]
-        if ti.static(self.bc_rho[3]==BC.fixedValue):
-            self.rho[x,self.ny-1,z] = self.rho_BC[3]
-        elif ti.static(self.bc_rho[3]==BC.zeroGradient):
-            self.rho[x,self.ny-1,z] = self.rho[x,self.ny-2,z]#2*self.rho[x,self.ny-2,z]-self.rho[x,self.ny-3,z]
-        if self.even_step[None]==0: # 偶数步
-            for s in ti.static([4,7,8]):
-                i_storage = self.periodic_index([x,self.ny-1,z]+self.e9[s])
-                i_neighbor_storage = self.periodic_index([x,self.ny-2,z]+self.e9[s])
-                self.f[i_storage][self.LR[s]] = self.feq9(s,x,self.ny-1,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,x,self.ny-2,z))
-        else:  # 奇数步
-            for s in ti.static([4,7,8]):
-                self.f[x,self.ny-1,z][s] = self.feq9(s,x,self.ny-1,z)+(self.f[x,self.ny-2,z][s]-self.feq9(s,x,self.ny-2,z))
+        if self.solid[x,self.ny-1,z]<1:
+            if ti.static(self.bc_v[3]==BC.fixedValue):
+                self.v[x,self.ny-1,z] = self.v_bc_profile[3][x,0,z]
+            elif ti.static(self.bc_v[3]==BC.zeroGradient):
+                self.v[x,self.ny-1,z] = self.v[x,self.ny-2,z]#2*self.v[x,self.ny-2,z]-self.v[x,self.ny-3,z]
+            if ti.static(self.bc_rho[3]==BC.fixedValue):
+                self.rho[x,self.ny-1,z] = self.rho_BC[3]
+            elif ti.static(self.bc_rho[3]==BC.zeroGradient):
+                self.rho[x,self.ny-1,z] = self.rho[x,self.ny-2,z]#2*self.rho[x,self.ny-2,z]-self.rho[x,self.ny-3,z]
+            if self.even_step[None]==0: # 偶数步
+                for s in ti.static([4,7,8]):
+                    i_storage = self.periodic_index([x,self.ny-1,z]+self.e9[s])
+                    i_neighbor_storage = self.periodic_index([x,self.ny-2,z]+self.e9[s])
+                    self.f[i_storage][self.LR[s]] = self.feq9(s,x,self.ny-1,z)+(self.f[i_neighbor_storage][self.LR[s]]-self.feq9(s,x,self.ny-2,z))
+            else:  # 奇数步
+                for s in ti.static([4,7,8]):
+                    self.f[x,self.ny-1,z][s] = self.feq9(s,x,self.ny-1,z)+(self.f[x,self.ny-2,z][s]-self.feq9(s,x,self.ny-2,z))
     """NEBB"""
     @ti.func
     def Boundary_condition_NEBB(self):
